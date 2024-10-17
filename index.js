4e00a2bb3ef20ddf0a03c594af76e2ea54c7d766
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-10-17 13:07:13 +0530

    one

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ mongoose.connect(process.env.MONGODB_URI)
 .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
-  res.send('blog backend');
+  res.send('TravelHunt Backend');
 });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-module.exports = app;
+// module.exports = app;
